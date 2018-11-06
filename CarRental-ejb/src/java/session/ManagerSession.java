af13eeea6c98625a2bfcdd1f55fa5ddb84129e06
maarten.sijmkens <maarten.sijmkens@gmail.com>
2018-11-06 12:45:11 +0100

    init

---
+++ CarRental-ejb/src/java/session/ManagerSession.java
@@ -0,0 +1,70 @@
+package session;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import rental.Car;
+import rental.CarRentalCompany;
+import rental.CarType;
+import rental.Reservation;
+
+import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+
+@Stateless
+public class ManagerSession implements ManagerSessionRemote {
+    
+    @PersistenceContext
+    EntityManager em;
+    
+    @Override
+    public Set<CarType> getCarTypes(String company) {
+        try {
+            return new HashSet<>(em.find(CarRentalCompany.class, company).getAllTypes());
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    @Override
+    public Set<Integer> getCarIds(String company, String type) {
+        Set<Integer> out = new HashSet<>();
+        try {
+            for(Car c: em.find(CarRentalCompany.class, company).getCars(type)){
+                out.add(c.getId());
+            }
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return out;
+    }
+
+    @Override
+    public int getNumberOfReservations(String company, String type, int id) {
+        try {
+            return em.find(CarRentalCompany.class, company).getCar(id).getReservations().size();
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+            return 0;
+        }
+    }
+
+    @Override
+    public int getNumberOfReservations(String company, String type) {
+        Set<Reservation> out = new HashSet<>();
+        try {
+            for(Car c: em.find(CarRentalCompany.class, company).getCars(type)){
+                out.addAll(c.getReservations());
+            }
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+            return 0;
+        }
+        return out.size();
+    }
+
+}
